51e89b9c8a5a62dc9649791782b1a397c4f10403
EvyaDev <106523004+EvyaDev@users.noreply.github.com>
2024-12-23 22:49:47 +0200

    add

--- front/hiddon/src/App.js
+++ front/hiddon/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Router from './Router';
 
-export const HOST = 'http://localhost:4000';
+export const HOST = 'https://hidon.onrender.com'
+
 
 function App() {
   return (
